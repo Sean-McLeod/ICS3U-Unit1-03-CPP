2b3caf0925f39f39b6dcda741bfeae03427c5af8
Ubuntu <ubuntu@ip-172-31-1-183.ec2.internal>
2020-11-20 19:20:36 +0000

    commit changes

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Sean McLeod All rights reserved
+
+// Created by Sean McLeod
+// Created on November 2020
+// This is a "Hello, World!" program with proper style
+
+
+#include <iostream>
+
+int main()  {
+    std::cout << "Hello, World" << std::endl;
+}
